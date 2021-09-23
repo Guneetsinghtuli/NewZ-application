484afa521ca99620cfdafa0bbd6a82c259e21141
wsameer <dev.wsameer@gmail.com>
2021-09-23 22:16:29 +0530

    refactor: converts to functional components for consistency

--- src/App.js
+++ src/App.js
@@ -1,51 +1,75 @@
-import React, { Component } from 'react'
-import Navbar from './components/Navbar'
-import News from './components/News'
-import LoadingBar from 'react-top-loading-bar'
-import {
-    BrowserRouter as Router,
-    Switch,
-    Route,
+import React, { useState } from "react";
+import Navbar from "./components/Navbar";
+import News from "./components/News";
+import LoadingBar from "react-top-loading-bar";
+import { BrowserRouter as Router, Switch, Route } from "react-router-dom";
 
-} from "react-router-dom";
+const App = () => {
+    const [progress, setProgress] = useState(10);
+    return (
+        <Router>
+            <LoadingBar color="#f11946" progress={progress} />
+            <Navbar />
+            <Switch>
+                <Route exact path="/">
+                    <News
+                        key="general"
+                        setProgress={setProgress}
+                        country="India"
+                        category="general"
+                    />
+                </Route>
+                <Route exact path="/entertainment">
+                    <News
+                        key="entertainment"
+                        setProgress={setProgress}
+                        country="India"
+                        category="entertainment"
+                    />
+                </Route>
+                <Route exact path="/business">
+                    <News
+                        key="business"
+                        setProgress={setProgress}
+                        country="India"
+                        category="business"
+                    />
+                </Route>
+                <Route exact path="/sports">
+                    <News
+                        key="sports"
+                        setProgress={setProgress}
+                        country="India"
+                        category="sports"
+                    />
+                </Route>
+                <Route exact path="/health">
+                    <News
+                        key="health"
+                        setProgress={setProgress}
+                        country="India"
+                        category="health"
+                    />
+                </Route>
+                <Route exact path="/science">
+                    <News
+                        key="science"
+                        setProgress={setProgress}
+                        country="India"
+                        category="science"
+                    />
+                </Route>
+                <Route exact path="/technology">
+                    <News
+                        key="technology"
+                        setProgress={setProgress}
+                        country="India"
+                        category="technology"
+                    />
+                </Route>
+            </Switch>
+        </Router>
+    );
+};
 
-export class App extends Component {
-    apiKey=process.env.REACT_APP_API_KEY;
-    constructor(){
-        super()
-        this.state = {
-            progress:10
-        }
-    }
-    setProgress =(progress)=>{
-        this.setState({progress:progress})
-    }
-
-    render() {
-        return (
-            <Router>
-                <LoadingBar
-                    color='#f11946'
-                    progress={this.state.progress}
-                    
-                />
-
-                <Navbar />
-                <Switch>
-                    <Route exact path="/"><News key="general" apiKey={this.apiKey} setProgress={this.setProgress}  country="India" category="general" /></Route>
-                    <Route exact path="/entertainment"><News key="entertainment" apiKey={this.apiKey} setProgress={this.setProgress} country="India" category="entertainment" /></Route>
-                    <Route exact path="/business"><News key="business" apiKey={this.apiKey} setProgress={this.setProgress} country="India" category="business" /></Route>
-                    <Route exact path="/sports"><News key="sports" apiKey={this.apiKey} setProgress={this.setProgress} country="India" category="sports" /></Route>
-                    <Route exact path="/health"><News key="health" apiKey={this.apiKey} setProgress={this.setProgress} country="India" category="health" /></Route>
-                    <Route exact path="/science"><News key="science" apiKey={this.apiKey} setProgress={this.setProgress} country="India" category="science" /></Route>
-                    <Route exact path="/technology"><News key="technology" apiKey={this.apiKey} setProgress={this.setProgress} country="India" category="technology" /></Route>
-                </Switch>
-
-
-            </Router>
-        )
-    }
-}
-
-export default App
-
+export default App;
