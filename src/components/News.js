ddf4182f528463c27e962d5cbdcb2cab3cccaae2
wsameer <dev.wsameer@gmail.com>
2021-09-24 16:19:00 +0530

    refactor: optimizes News component

--- src/components/News.js
+++ src/components/News.js
@@ -4,51 +4,38 @@ import PropTypes from "prop-types";
 import InfiniteScroll from "react-infinite-scroll-component";
 import Loader from "./Loader";
 import { API_KEY, BASE_API_URL } from "../constants";
+import { capitalize } from "../utils";
 
-const News = (props) => {
+const News = React.memo((props) => {
     const { setProgress, country, category } = props;
     const [news, setNews] = useState([]);
     const [currentPage, setCurrentPage] = useState(1);
     const [totalPages, setTotalPages] = useState(1);
 
-    const capitalise = (str) => str.slice(0, 1).toUpperCase() + str.slice(1);
-    const _setProgress = useCallback((val) => setProgress(val), [setProgress]);
-    const updateNews = useCallback(async () => {
-        _setProgress(30);
-        const url = `${BASE_API_URL}?q=${country}&apiKey=${API_KEY}&page=1&pageSize=10&language=en&category=${category}`;
-        const data = await fetch(url);
-        const { status, code, articles, totalResults } = await data.json();
-        if (status === "error") {
-            console.log("[Network error] code:", code);
-        } else {
-            setTotalPages(totalResults);
-            _setProgress(70);
-            setNews(articles);
-        }
-        _setProgress(100);
-        document.title = `NewZ-${capitalise(category)}`;
-    }, [country, category, _setProgress]);
-
-    const fetchMoreData = async () => {
-        _setProgress(30);
-        const url = `${BASE_API_URL}?q=${country}&apiKey=${API_KEY}&page=${
-            currentPage + 1
-        }&pageSize=10&language=en&category=${category}`;
-        let newdata = await fetch(url);
-        const { status, code, articles } = await newdata.json();
-        if (status === "error") {
-            console.log("[Network error] code:", code);
-        } else {
-            _setProgress(70);
-            setNews(news.concat(articles));
-            setCurrentPage(currentPage + 1);
-        }
-        _setProgress(100);
-    };
+    const _setProgress = useCallback((val) => setProgress(val), [setProgress]),
+        incrementCurrentPage = useCallback(
+            () => setCurrentPage((prev) => prev + 1),
+            []
+        ),
+        getNews = useCallback(async () => {
+            _setProgress(30);
+            const url = `${BASE_API_URL}?q=${country}&apiKey=${API_KEY}&page=${currentPage}&pageSize=10&language=en&category=${category}`;
+            const data = await fetch(url);
+            const { status, code, articles, totalResults } = await data.json();
+            if (status === "error") {
+                console.log("[Network error] code:", code);
+            } else {
+                _setProgress(70);
+                setTotalPages(totalResults);
+                setNews((prev) => prev.concat(articles));
+            }
+            _setProgress(100);
+            document.title = `NewZ-${capitalize(category)}`;
+        }, [country, currentPage, category, _setProgress]);
 
     useEffect(() => {
-        updateNews();
-    }, [updateNews]);
+        getNews();
+    }, [getNews]);
 
     return (
         <>
@@ -56,23 +43,29 @@ const News = (props) => {
                 <h1>Trending News</h1>
                 <InfiniteScroll
                     dataLength={news.length - 1}
-                    next={fetchMoreData}
+                    next={incrementCurrentPage}
                     hasMore={news.length < totalPages}
                     loader={<Loader />}
                 >
                     <div className="row my-3">
                         {news.map(
-                            ({
-                                title,
-                                description,
-                                urlToImage,
-                                url,
-                                author,
-                                publishedAt,
-                                source: { name },
-                            }) => {
+                            (
+                                {
+                                    title,
+                                    description,
+                                    urlToImage,
+                                    url,
+                                    author,
+                                    publishedAt,
+                                    source: { name },
+                                },
+                                index
+                            ) => {
                                 return (
-                                    <div className="col-md-4 my-3 " key={url}>
+                                    <div
+                                        className="col-md-4 my-3 "
+                                        key={index.toString()}
+                                    >
                                         <Newsitem
                                             title={title}
                                             content={description}
@@ -91,7 +84,7 @@ const News = (props) => {
             </div>
         </>
     );
-};
+});
 
 News.protoype = {
     country: PropTypes.string,
